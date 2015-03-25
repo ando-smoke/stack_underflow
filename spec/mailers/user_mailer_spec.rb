68cb17f224c745ca0620d0179f8e38eccfd8c92e
Andy Uppendahl <a.uppendahl@gmail.com>
2015-03-25 09:33:47 -0700

    Correct user_mailer_spec by sending req'd 'question' arg to response_notification method.

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,16 +2,22 @@ require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
   describe "response_notification" do
-    let(:mail) { UserMailer.response_notification }
+
+    # 3.25.15: add FactoryGirl creation of a question object
+    # to make below call to UserMailer.response_notification method pass
+    # with required single arg (i.e. a question). AU
+    let(:mail) {
+      UserMailer.response_notification(FactoryGirl.create(:question))
+    }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Response notification")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.subject).to eq("New response received")
+      expect(mail.to).to eq(["thatswhatshesaid@yomama.com"])
+      expect(mail.from).to eq(["info-daemon@stackunderflow.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to include("You have received a new response")
     end
   end
 
